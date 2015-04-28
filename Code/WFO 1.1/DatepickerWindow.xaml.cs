ab5cddf522dad18025a7c9fea362c63c8b16e848
Shane Cunningham <mayhem2277@gmail.com>
2015-04-28 19:27:27 +0100

    added new datepicker window
    New extra option window, updated colour scheme. new faster perl scripts

---
+++ Code/WFO 1.1/DatepickerWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WFO_PROJECT
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        string startDateValue;
+        string endDateValue;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Date_picker_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            
+            startDateValue = e.NewValue.ToString();
+        }
+
+        private void Date_pickertwo_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+             endDateValue = e.NewValue.ToString();
+        }
+
+        public string startOne
+        {
+            set { startDateValue = startDateValue; }
+            get { return startDateValue; }
+            
+        }
+
+        public  string endOne
+        {
+            set { endDateValue = endDateValue; }
+            get { return endDateValue; }
+           
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Windowpopup.Close();
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Date_picker.IsEnabled = true;
+            Date_pickertwo.IsEnabled = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Date_picker.IsEnabled = false;
+            Date_pickertwo.IsEnabled = false;
+
+        }
+
+        private void ExcludeCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            ExcludeTxtBox.IsEnabled = true;
+        }
+
+        private void ExcludeCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ExcludeTxtBox.IsEnabled = false;
+        }
+
+        private void SwapCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            SwapTxtBox.IsEnabled = true;
+        }
+
+        private void SwapCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            SwapTxtBox.IsEnabled = false;
+        }
+
+    }
+}
---
+++ Code/WFO 1.1/NewScriptWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using Xceed.Wpf.Toolkit;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace WFO_PROJECT
+{
+    /// <summary>
+    /// Interaction logic for NewScriptWindow.xaml
+    /// </summary>
+    public partial class NewScriptWindow : Window
+    {
+        public NewScriptWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> scriptList = new List<string>();
+            string line;
+            var fileName = Directory.GetCurrentDirectory() + "\\ListViewScriptsTwo.txt";
+            System.IO.StreamReader file_open = new System.IO.StreamReader(fileName);
+            while ((line = file_open.ReadLine()) != null)
+            {
+                Regex regex = new Regex("name :");
+                if (regex.IsMatch(line))
+                {
+                    string[] words = line.Split(':');
+                    scriptList.Add(words[1]);
+                }
+            }
+            file_open.Close();
+
+            string newScriptName = scriptNamebox.Text;
+
+            if (string.IsNullOrEmpty(scriptNamebox.Text))
+            {
+                MessageBox.Show("You must enter a name for the new script first.");
+                return;
+            }
+            else
+            {
+
+
+                foreach (string words in scriptList)
+                {
+                    if (words == newScriptName)
+                    {
+                        MessageBox.Show("This name is already used");
+                        return;
+                    }
+                }
+
+            }
+
+            string temp_File = Directory.GetCurrentDirectory() + "\\ListViewScriptsTwo.txt";
+            StreamWriter sWriter = File.AppendText(temp_File);
+            sWriter.Write("name :" + newScriptName);
+            sWriter.Write("\r\n");
+            sWriter.Write("--");
+            sWriter.Write("\r\n");
+
+            sWriter.Close();
+            NewScriptWindow1.Close();
+        }
+    }
+}
